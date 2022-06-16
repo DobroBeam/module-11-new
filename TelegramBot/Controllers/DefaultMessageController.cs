b7ba3a3e4fec4e547b91223a1a6c6eaf7b8ae187
Dmitriy Lazarenko <fondango@inbox.ru>
2022-06-16 20:44:21 +0300

    new buttons

--- TelegramBot/Controllers/DefaultMessageController.cs
+++ TelegramBot/Controllers/DefaultMessageController.cs
@@ -19,6 +19,8 @@ namespace TelegramBot.Controllers
         }
         public async Task Handle(Message message, CancellationToken ct)
         {
+
+
             Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
             await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено сообщение не поддерживаемого формата", cancellationToken: ct);
         }
